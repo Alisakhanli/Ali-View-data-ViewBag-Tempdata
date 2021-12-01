aab6fabb0de3f1c0e22d9c4682bc2be96d6fe730
Ali <65233907+Alisakhanli@users.noreply.github.com>
2021-12-01 18:57:16 +0400

    Add files via upload

---
+++ homework/homework/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            if(id== null)
+            {
+                ViewData["Salam qaqas"] = "Id is null";
+                return View("NewList");
+            }
+            return Json(new { Name = "Ayxan", Surname = "Abdullayev", Age = 21 });
+        }
+    }
+}
